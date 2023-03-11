b5d46883bebdd34a77d266b8b339f080a5b0494e
gabri <gabrielbarcel08@gmail.com>
2023-03-11 13:53:42 -0300

    Adicionando arquivos.

---
+++ ex002.py
@@ -0,0 +1,8 @@
+print('Olá, por favor, preencha os campos abaixo de acordo com sua data de nascimento:')
+dia = input('Dia:')
+mês = input('Mês:')
+ano = input('Ano:')
+print('Você nasceu no dia', dia, 'de', mês, 'de', ano, '. Correto?')
+
+
+
---
+++ ex003.py
@@ -0,0 +1,9 @@
+print('Insira os números para soma:')
+num1 = input('Primeiro Número:')
+num2 = input('Segundo Número:')
+print('A soma é', int(num1) + int(num2), '.')
+
+
+
+
+
